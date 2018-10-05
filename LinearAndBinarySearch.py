1e1b37f29a0ea9757e55f30279f5f58c48a7a16b
Dipesh Paul <dipeshpal17@gmail.com>
2018-10-05 23:30:51 +0530

    Update LinearAndBinarySearch.py

--- LinearAndBinarySearch.py
+++ LinearAndBinarySearch.py
@@ -1,9 +1,9 @@
+#BinarySearch
 def binarySearch(seq,key):
     lo = 0
     hi = len(seq)-1
     le = len(seq)
-   
-    
+ 
     while hi>=lo:
         mid = lo+(hi-lo)/2
         mid = int(mid)
@@ -15,12 +15,28 @@ def binarySearch(seq,key):
             return mid
     return -1
 
-        
+#LinearSearch
+def linearSearch(seq, key):
+    le = len(seq)
+    for i in range(le):
+        if key==seq[i]:
+            return i
+    return -1 
+    
 key = input("Enter element to find: ")
 key = int(key)
 seq = [4,5,7,8,9]
-res = binarySearch(seq, key)
-if res>-1:
-    print("Element found at index: ",res)
+ch = input("Press 1 for Binary Search, 2 for Linear Search: ")
+ch = int(ch)
+
+if ch==1:
+    res = binarySearch(seq, key)
+if ch==2:
+    res = linearSearch(seq, key)
+
+if res > -1:
+    print("Element found at index: ", res)
+    print("Complexity of Linear Search is O(n)")
+    print("Complexity of Binart Search is O(logn)")
 else:
     print("Element not found")
